140ffac95fdc8f5963c06de97d8cad47327f1fd5
Tanto <99785440+Egruda@users.noreply.github.com>
2023-06-18 14:04:05 +0700

    Testing github server

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css';
 import activateListener from './expand.js';
 import display from './dom.js';
+import { storageAvailable } from './storage';
+import { projectArray } from './project';
+import addIcon from './add.png';
 
 
 
+if (storageAvailable("localStorage")) {
+    const storedProjects = JSON.parse(localStorage.getItem('projects'));
+    projectArray = storedProjects;
+  } else {
+    projectArray = [Training, Studying];
+  }
 
 
 display();
